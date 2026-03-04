639b42551d2e523fce26ba1cfb47bfc42ebce8c0
zirobtc <bentenroben35@gmail.com>
2026-03-04 09:32:33 +0100

    fix tables

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,3 +6,20 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y --profile minimal
 cargo --version
 rustc --version
 
+
+cypher-shell -u neo4j -p neo4j123 \
+  "CREATE CONSTRAINT wallet_address IF NOT EXISTS FOR (w:Wallet) REQUIRE w.address IS UNIQUE;"
+
+cypher-shell -u neo4j -p neo4j123 \
+  "CREATE CONSTRAINT token_address IF NOT EXISTS FOR (t:Token) REQUIRE t.address IS UNIQUE;"
+
+#   clickhouse-client \
+#     --query "INSERT INTO known_wallets FORMAT TabSeparatedWithNames" \
+#     < known_wallets.tsv
+
+# hf download zirobtc/pump-fun-dataset \
+#   token_metrics_latest_epoch_851.parquet \
+#   wallet_profiles_epoch_851.parquet \
+#   wallet_profile_metrics_latest_epoch_851.parquet \
+#   wallet_holdings_latest_epoch_851.parquet \
+#   --local-dir .
